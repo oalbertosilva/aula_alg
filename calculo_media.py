f08f23d49c10bd5ed132a48d37d6f65c05b3d078
Gian Oliveira <user@MacBook-Air-de-User.local>
2025-02-19 12:30:30 -0400

    Fiz o while funcionar

--- calculo_media.py
+++ calculo_media.py
@@ -1,7 +1,16 @@
-repeat = "N"
+repeat = "S"
 while (repeat == "S"):
+
     n1 = float(input("Digite a primeira nota: "))
     n2 = float(input("Digite a segunda nota: "))
     n3 = float(input("Digite a terceira nota: "))
     media = (n1 + n2 + n3) / 3
-    print("A máedia obtida foi: ", media)
+    print("A média obtida foi: ", media)
+    if media >= 6.0:
+        print("Aluno aprovado!")
+    else:
+        print("O aluno não obteve a média necessária. Aluno reprovado!")
+
+    repeat = input(("quer continuar? S/N: "))
+    if repeat == "N":
+        print("Fim do programa!")
